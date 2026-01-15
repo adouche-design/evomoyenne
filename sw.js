46efe71a865ae90e304224513c8ccf8e9cdaf624
Adouche <mr.froid.84@gmail.com>
2026-01-15 08:10:03 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'evomoyenne';
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll([
+            '/', 
+            '/index.html', 
+            '/assets/css/style.css', 
+            '/assets/js/app.js'
+        ]))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
